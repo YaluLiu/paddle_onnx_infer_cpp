74bb33886180a813ff5497d97ca0d116b97824c4
boli-shixi <yalu@changchunboli.com>
2024-03-05 16:36:06 +0800

    style:#5

--- README.md
+++ README.md
@@ -5,6 +5,7 @@ This example demonstrates how to perform inference using YOLOv8-onnx-models,now 
 [ultralytics-v8.1](https://github.com/ultralytics/ultralytics)
 
 
+
 ## Usage
 
 ```bash
@@ -13,6 +14,10 @@ bash run.sh download_onnx_runtime
 bash run.sh build
 bash run.sh run
 ```
+
+## more version of onnx-runtime-lib
+[download_url](https://github.com/microsoft/onnxruntime/releases)
+
 
 ## your own 
 if you want to load your own onnx model, may need [onnx-runtime](https://github.com/microsoft/onnxruntime)
--- run.sh
+++ run.sh
@@ -5,8 +5,9 @@ project_dir=$(cd "$(dirname "$0")";pwd)
 
 function download_onnx_runtime(){
   onnx_version="1.16.3"
-  wget https://github.com/microsoft/onnxruntime/releases/download/v${onnx_version}/onnxruntime-linux-x64-gpu-${onnx_version}.tgz
-  tar -zxvf onnxruntime-linux-x64-gpu-${onnx_version}.tgz
+  os="linux" # win
+  wget https://github.com/microsoft/onnxruntime/releases/download/v${onnx_version}/onnxruntime-${os}-x64-gpu-${onnx_version}.tgz
+  tar -zxvf onnxruntime-${os}-x64-gpu-${onnx_version}.tgz
   # for save visualize result
   mkdir result
 }
